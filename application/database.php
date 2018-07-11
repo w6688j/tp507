0c1ed8a1adea25f5cac393328318be4f76127f7a
root <953372680@qq.com>
2018-07-11 19:07:12 +0800

    修改数据库配置

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '106.14.176.10',
+    'hostname'        => '47.105.110.226',
     // 数据库名
     'database'        => 'zerg',
     // 用户名
